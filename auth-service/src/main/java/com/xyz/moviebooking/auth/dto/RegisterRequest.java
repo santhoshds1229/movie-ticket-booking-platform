767f80694fc016739b57449820bbe22849ac2c0c
santhoshds1229 <santhoshds12@gmail.com>
2026-04-10 14:13:08 +0530

    feat: Add RegisterRequest, AuthRequest, AuthResponse DTOs and UserRepository

---
+++ auth-service/src/main/java/com/xyz/moviebooking/auth/dto/RegisterRequest.java
@@ -0,0 +1,37 @@
+package com.xyz.moviebooking.auth.dto;
+
+import com.xyz.moviebooking.auth.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    private String phone;
+
+    @NotNull(message = "Role is required")
+    private User.Role role;
+}
